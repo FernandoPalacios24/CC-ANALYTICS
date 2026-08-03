6f6c123afe7a421ba7f3bacb972842e670080850
FERNANDO PALACIOS <updateconfirm17@gmail.com>
2026-08-03 11:59:54 -0600

    Activar edición directa y metas en ingreso de ventas

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { AnalyticsRoot } from "@/components/analytics-root";
 import { InitialDashboardController } from "@/components/initial-dashboard-controller";
+import { SalesDataEnhancementController } from "@/components/sales-data-enhancement-controller";
 
 export default function Home() {
   return (
     <>
       <AnalyticsRoot />
       <InitialDashboardController />
+      <SalesDataEnhancementController />
     </>
   );
 }
